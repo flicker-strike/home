50d47038380a049a60aabdd7d746cc829f341bc9
Rehrocko <blixanl20@gmail.com>
2026-01-20 22:54:43 -0600

    test fix #1/2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://flicker-strike.github.io',
   base: '/home',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
